b296e6d83d002ffc9b816166a795d2f059078c65
Khan-Sg <abuzar.ahamed.khan@gmail.com>
2025-11-13 09:02:59 +0530

    Links

---
+++ src/main/java/com/sgtesting/Selenium/LinksAmazon.java
@@ -0,0 +1,24 @@
+package com.sgtesting.Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class LinksAmazon {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+        List<WebElement> list=driver.findElements(By.tagName("a"));
+        for(WebElement lists:list){
+            System.out.println(lists.getText()+" "+lists.getAttribute("href"));
+
+        }
+        driver.quit();
+    }
+}
