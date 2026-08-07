e6e80392edf4a515394f2828142dbd0f02f6ce73
Khan-Sg <abuzar.ahamed.khan@gmail.com>
2026-08-07 12:53:56 +0530

    gold

--- src/test/java/com/sgtesting/Practise/Ambika.java
+++ src/test/java/com/sgtesting/Practise/Ambika.java
@@ -1,8 +1,11 @@
 package com.sgtesting.Practise;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
